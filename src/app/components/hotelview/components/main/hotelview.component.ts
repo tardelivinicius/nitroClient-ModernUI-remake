815e8b7aaa80cf7f548cab9bc106a013ce2f0385
Vinícius Valle <tardelivinicius@gmail.com>
2022-03-09 23:07:45 -0300

    Added HotelViewBackground Right and Left

--- src/app/components/hotelview/components/main/hotelview.component.ts
+++ src/app/components/hotelview/components/main/hotelview.component.ts
@@ -10,6 +10,8 @@ export class HotelViewComponent
 {
     private _background: string;
     private _backgroundColour: string;
+    private _hotelViewLeft: string;
+    private _hotelViewRight: string;
     private _sun: string;
     private _drape: string;
     private _left: string;
@@ -25,6 +27,8 @@ export class HotelViewComponent
     {
         this._background        = Nitro.instance.core.configuration.interpolate(Nitro.instance.getConfiguration('hotelview.images')['background']);
         this._backgroundColour  = Nitro.instance.getConfiguration('hotelview.images')['background.colour'];
+        this._hotelViewLeft  = Nitro.instance.getConfiguration('hotelview.images')['hotelview.left'];
+        this._hotelViewRight  = Nitro.instance.getConfiguration('hotelview.images')['hotelview.right'];
         this._sun               = Nitro.instance.core.configuration.interpolate(Nitro.instance.getConfiguration('hotelview.images')['sun']);
         this._drape             = Nitro.instance.core.configuration.interpolate(Nitro.instance.getConfiguration('hotelview.images')['drape']);
         this._left              = Nitro.instance.core.configuration.interpolate(Nitro.instance.getConfiguration('hotelview.images')['left']);
@@ -50,6 +54,16 @@ export class HotelViewComponent
     public get backgroundColour(): string
     {
         return (this._backgroundColour || null);
+    }
+
+    public get hotelViewLeft(): string
+    {
+        return (this._hotelViewLeft || null);
+    }
+
+    public get hotelViewRight(): string
+    {
+        return (this._hotelViewRight || null);
     }
 
     public get sun(): string
